ff15f570ff85bde8de066b63104eed28602dc006
Dylan Frank <drfrank22@gmail.com>
2020-09-29 02:03:47 -0400

    current weather conditions appear for city

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ var apiKey = "&appid=9329c7236e5aabd423e23ac6e00d8109";
 var city = $("#searchBar").val();
 
 
-$("#searchBar").keypress(function(event) { 
-	if (event.keyCode === 13) { 
-		event.preventDefault();
-		$("#searchBtn").on("click"); 
-	} 
-});
+// $("#searchBar").keypress(function(event) { 
+// 	if (event.keyCode === 13) { 
+// 		event.preventDefault();
+// 		$("#searchBtn").on("click"); 
+// 	} 
+// });
 
 // Submit Button On Click Event
 $("#submitBtn").on("click", function() {
@@ -22,5 +22,62 @@ $("#submitBtn").on("click", function() {
     .then(function (response){
         console.log(response);
         console.log(response.name)
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            console.log(Math.floor(tempF))
+            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            tempF = Math.floor(tempF);
+            $(cardBody).empty();
+            // var card = $("#card");
+            var cardBody = $("#currentConditions");
+            $("#weatherIcon").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+            var currentCity = $("#currentCity").text(response.name);
+            var date = $("#currentDate").text(moment().format("MMMM Do YYYY"));
+            var currentTemp = $("#currentTemp").text("Temperature: " + tempF + " °F");
+            var currentHumidity = $("#currentHumidity").text("Humidity: " + response.main.humidity + "%");
+            var currentWindSpeed = $("#currentWindSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
+        
+            cardBody.append(currentCity, date, currentTemp, currentHumidity, currentWindSpeed);
+        
     });
+
+    
+
+
+
+
+    // Call Functions
+    // showCurrentConditions();
+    // showForecast();
+    // showSearchList();
 });
+
+// Function displaying current weather conditions.
+// function showCurrentConditions (response) {
+//     var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+//     tempF = Math.floor(tempF);
+//     $(cardBody).empty();
+//     // var card = $("#card");
+//     var cardBody = $("#currentConditions");
+//     $("#weatherIcon").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+//     var currentCity = $("#currentCity").text(response.name);
+//     var date = $("#currentDate").text(moment().format("MMMM Do YYYY"));
+//     var currentTemp = $("#currentTemp").text("Temperature: " + tempF + " °F");
+//     var currentHumidity = $("#currentHumidity").text("Humidity: " + response.main.humidity + "%");
+//     var currentWindSpeed = $("#currentWindSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
+
+//     cardBody.append(currentCity, date, currentTemp, currentHumidity, currentWindSpeed);
+
+
+   
+
+// }
+
+// Function displaying 5 day forecast.
+// function showForecast (response) {
+    
+// }
+
+// // Function prepending search items in list.
+// function showSearchList () {
+    
+// }
