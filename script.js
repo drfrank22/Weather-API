10e52921d8bf8ecd672acc3dc4caf659bd6a2d7e
Dylan Frank <drfrank22@gmail.com>
2020-09-29 00:27:04 -0400

    console logged object returned from AJAX

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-var apiKey = "9329c7236e5aabd423e23ac6e00d8109";
-// var searchedCity = 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +  + apiKey;
+var apiKey = "&appid=9329c7236e5aabd423e23ac6e00d8109";
+var city = $("#searchBar").val();
+
+
+$("#searchBar").keypress(function(event) { 
+	if (event.keyCode === 13) { 
+		event.preventDefault();
+		$("#searchBtn").on("click"); 
+	} 
+});
+
+// Submit Button On Click Event
+$("#submitBtn").on("click", function() {
+    event.preventDefault();
+    city = $("#searchBar").val();
+    $("#searchBar").val("");
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    })
+    .then(function (response){
+        console.log(response);
+        console.log(response.name)
+    });
+});
